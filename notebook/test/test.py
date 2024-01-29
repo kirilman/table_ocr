1102d6e25120c0fdd41ec6fb0434da0d69281002
kirilman <kirilman03@gmail.com>
2024-01-29 12:07:59 +0500

    u test

--- notebook/test/test.py
+++ notebook/test/test.py
@@ -1,9 +1,13 @@
-from main import Table
+from main import sum
 import cv2
 import numpy as np
 
 
-def test_Table():
-    table = Table()
-    img = cv2.imread("../good/230913050_page_0.jpg")
-    assert type(img, np.array)
+def test_sum():
+    assert sum(5, 5) == 10
+
+
+# def test_Table():
+#     table = Table()
+#     img = cv2.imread("../good/230913050_page_0.jpg")
+#     assert type(img, np.array)
